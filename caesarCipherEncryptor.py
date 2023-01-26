98a6054a41d112bb65a035f79e98df13f261a5eb
MirageM <imiragesz@gmail.com>
2023-01-25 22:39:20 -0500

    ceasar

--- caesarCipherEncryptor.py
+++ caesarCipherEncryptor.py
@@ -1,4 +1,14 @@
-#Redo
+#O(n) time | O(n) space
+#chr -> A | ord -> 65
 def caesarCipherEncryptor(string, key):
     newLetters = []
     newKey = key % 26
+    for letter in string:
+        newLetters.append(getNewLetter(letter, newKey))
+    return "".join(newLetters)
+
+def getNewLetter(letter, newKey):
+    newLetterCode = ord(letter) + newKey
+    return chr(newLetterCode) if newLetterCode <= 122 else chr(96 + newLetterCode % 122)
+    
+    
